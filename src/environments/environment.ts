803b9e476c8350edfd3ded631e32f424c599bea0
aplatov76 <it@crb29.ru>
2021-10-25 10:09:32 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  url: 'http://localhost:3000/api'
+  production: true,
+  url: '/api',
 };
